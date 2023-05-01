2e6447c1ab7952c59fa4f88f77b096edce74352e
Mac <mac@Macs-MacBook-Air.local>
2023-05-02 01:13:39 +0600

    initial route set

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8H53Rq_GqBHAK-7jIxbXsrpO5NpGiFsw",
+  authDomain: "fir-assignment-10-d2367.firebaseapp.com",
+  projectId: "fir-assignment-10-d2367",
+  storageBucket: "fir-assignment-10-d2367.appspot.com",
+  messagingSenderId: "962055675298",
+  appId: "1:962055675298:web:73349a11e6af745cd4a6fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
